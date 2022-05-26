a760c448632e117a18272b1e9d51ab8159134371
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-26 23:33:24 +0600

    deployed to google cloud for hosting

--- src/components/Dashboard/Prescriptions/Prescriptions.js
+++ src/components/Dashboard/Prescriptions/Prescriptions.js
@@ -1,21 +1,20 @@
-import { Grid } from '@material-ui/core';
-import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
+import { Grid } from "@material-ui/core";
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Prescriptions = () => {
-    return (
-        <div>
-           <Grid container spacing={2}>
-            <Grid  item xs={12} md={5} lg={2}>
-        <Sidebar></Sidebar>
-                </Grid>
-            <Grid item xs={12} md={5} lg={10}>
-            <h1 className="text-center">Prescriptions</h1>
-            
-                </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={5} lg={2}>
+          <Sidebar></Sidebar>
+        </Grid>
+        <Grid item xs={12} md={5} lg={10}>
+          <h1 className="text-center">Prescriptions</h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Prescriptions;
